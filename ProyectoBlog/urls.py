630e18494315b34c7f877ca05571ba09617f2d11
LucasProsdocimo <lucas.prosdocimo@gmail.com>
2022-05-29 21:50:00 -0300

    commit

--- ProyectoBlog/urls.py
+++ ProyectoBlog/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('AppBlog/', include('AppBlog.urls'))
+    path('', include('AppBlog.urls'))
 ]
